116bb23552a8f9bde47a3646ea5ed93d4468a055
GustavoIsAway <pessoal.gustavojr@gmail.com>
2024-11-10 01:47:33 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+while True:
+    url = input("Introduza um link de perfil do Bluesky: ")
+
+    if not "https://bsky.app/profile/" in url:
+        print("Link inválido. Tente novamente.")
+    else:
+        break
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0" 
+}
+requisicao = requests.get(url, headers=headers)
+
+bluesky_page = BeautifulSoup(requisicao.text, "html.parser")
+rss_page = requests.get(bluesky_page.find("link", rel="alternate")["href"], headers=headers)
+rss_elements = BeautifulSoup(rss_page.text, "html.parser")
+file = open("postagens.txt", "w+")
+
+
+for post in rss_elements.find_all("description"):
+    file.write(f"{post.get_text()}\n")
+
+print("Processo finalizado. Cheque o diretório do script por 'postagens.txt'.")
